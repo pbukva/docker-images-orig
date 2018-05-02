8e2c2a2fbf50c72085fcdd6de8a8d5a898635f4f
Peter Bukva <peter.bukva@fetch.ai>
2018-05-02 11:38:14 +0100

    Ensuring docker registry credentials settings for jenkins CI scripts

--- scripts/jenkins-ci-docker-build-img.sh
+++ scripts/jenkins-ci-docker-build-img.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
-set -e
+#!/bin/bash -ex
+
+./scripts/docker-add_registry-cred-helpers.py
 
 ./scripts/docker-delete-img-from-cache.sh
 ./scripts/docker-build-img.sh --no-cache --
+
+if [[ "$PUBLISH_IMAGE_TO_DOCKER_REGISTRY" == "true" ]]
+then
+    ./scripts/docker-publish-img.sh
+else
+    echo "The image has *NOT* been pushed to the docker registry because PUBLISH_IMAGE_TO_DOCKER_REGISTRY=$PUBLISH_IMAGE_TO_DOCKER_REGISTRY." 
+fi
+
+./scripts/docker-delete-img-from-cache.sh
 
